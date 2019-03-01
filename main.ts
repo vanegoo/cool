f1065c46ec6263e8854f4fb32bcaecbd375560cf
vangun <1262827358@qq.com>
2019-03-01 17:45:25 +0800

    add WalkLine blocks

--- main.ts
+++ main.ts
@@ -64,70 +64,470 @@ enum servo_ports {
     J4,
 }
 
+enum CmpStr_dir {
+    //% block="ToBefore"
+    ToBefore,
+    //% block="ToAfter"
+    ToAfter
+}
+
 /**
  * Coolguy basic extension
  */
 //% weight=100 color=#ffc500 icon="\uf17b"
-//% groups=['GraySensors', 'NixieTube', 'IRremote', 'UltrasoundWave', 'Motors', others']
+//% groups=['CmpStr', WalkLine', 'NixieTube', 'IRremote', 'UltrasoundWave', 'Motors', others']
 namespace Coolguy_basic {
+    //----------------------字符串比较---------------------------------
+    let Cmpstring = "";
+    
+    /**
+     * Set the compared string
+     * @param str the string as comparsion, eg: hello
+     */
+    //% blockId=CmpStr_SetString
+    //% block="Set the compared string %str|"
+    //% group=CmpStr
+    export function CmpStr_SetString(str: string) {
+        Cmpstring = str;
+    }
 
-    //----------------------五灰度-----------------------------------
-    let SensorGray_pin: DigitalPin;
+    /**
+     * Get the compared string
+     */
+    //% blockId=CmpStr_GetString
+    //% block="Get the compared string"
+    //% group=CmpStr
+    export function CmpStr_GetString(): string {
+        return Cmpstring;
+    }
+
+    /**
+     * Is the compared string same as expected? 
+     */
+    //% blockId=CmpStr_StringEqual
+    //% block="Is the compared string same as %str|"
+    //% group=CmpStr
+    export function CmpStr_StringEqual(str: string): boolean {
+        if(str === Cmpstring)
+            return true;    
+        
+        return false;
+    }
+
+    /**
+     * Is the string included? 
+     */
+    //% blockId=CmpStr_IncludeString
+    //% block="Do the compared string include %str|"
+    //% group=CmpStr
+    export function CmpStr_IncludeString(str: string): boolean {     
+        if(Cmpstring.includes(str))
+            return true;
+        
+        return false;
+    }
+
+    /**
+     * Get the number from the compared string
+     */
+    //% blockId=CmpStr_Content_ToInt
+    //% block="Get the number %String_part| %position|from the compared string"
+    //% group=CmpStr
+    export function CmpStr_Content_ToInt(String_part: string, position: CmpStr_dir): number {
+        let s_position = Cmpstring.indexOf(String_part);
+
+        if(s_position == -1)
+            return -1;
+        else 
+        {
+            let comdata = "";
+            let clocktime = 0;
+            let times = 1;
+            let num = "";
+
+            if(position == CmpStr_dir.ToBefore) {
+                comdata = Cmpstring.substr(0, s_position);
+                for(let i = s_position - 1; i >= 0; i --) {
+                    num = comdata.charAt(i);
+                    if( num >= '0' && num <= '9') {
+                        clocktime += (num.charCodeAt(0) - '0'.charCodeAt(0)) * times;
+                        times *= 10;
+                    }
+                    else if(i == s_position - 1)
+                        return -1;
+                    else
+                        break;	
+                }
+                return clocktime;
+            }
+            else {
+                comdata = Cmpstring.substr( s_position + String_part.length, Cmpstring.length );
+                for(let i = 0; i < comdata.length; i ++) {
+                    num = comdata.charAt(i);
+                    if( num >= '0' && num <= '9') {
+                        clocktime *= 10;
+                        clocktime += (num.charCodeAt(0) - '0'.charCodeAt(0));
+                    }
+                    else if(i == 0)
+                        return -1;
+                    else
+                        break;	
+                }
+                return clocktime;
+            }
+        }
+    }
+
+    /**
+     * Get the string from the compared
+     */
+    //% blockId=CmpStr_Content_ToString
+    //% block="Get the string %String_part| %position|from the compared string"
+    //% group=CmpStr
+    export function CmpStr_Content_ToString(String_part: string, position: CmpStr_dir): string {
+        let s_position = Cmpstring.indexOf(String_part);
+        let comdata = "";
+
+        if(s_position == -1)
+            comdata = "NULL";
+        else {
+            if(position == CmpStr_dir.ToBefore)
+                comdata = Cmpstring.substr(0, s_position);
+            else
+                comdata = Cmpstring.substr( s_position + String_part.length, Cmpstring.length );
+        }
+        return comdata;
+    }
+
+    //----------------------循线模块-----------------------------------
+    let SensorGray_SCL: DigitalPin;
+    let SensorGray_SDA: DigitalPin;
     let DATA_BUF = [0, 0, 0, 0, 0, 0];
+    let CmpValue_BUF = [0, 0, 0, 0, 0, 0];
 
     /**
      * GraySensors init(Except J1)
      */
     //% blockId=SensorGray_Init
     //% block="Set GraySensors at %exterpin|"
-    //% group=GraySensors
+    //% group=WalkLine
     //% exterpin.fieldEditor="gridpicker" exterpin.fieldOptions.columns=2
     //% exterpin.fieldOptions.tooltips="false" exterpin.fieldOptions.width="150"
-    export function SensorGray_Init(exterpin: exter_ports) {
+    export function SensorGray_Init(exterpin: exter_ports1) {
         switch (exterpin) {
-            case exter_ports.J1:
-                SensorGray_pin = DigitalPin.P0;
+            case exter_ports1.J5:
+                SensorGray_SCL = DigitalPin.P13;
+                SensorGray_SDA = DigitalPin.P14;
                 break;
-            case exter_ports.J2:
-                SensorGray_pin = DigitalPin.P1;
-                break;
-            case exter_ports.J3:
-                SensorGray_pin = DigitalPin.P2;
-                break;
-            case exter_ports.J4:
-                SensorGray_pin = DigitalPin.P16;
-                break;
-            case exter_ports.J5:
-                SensorGray_pin = DigitalPin.P13;
-                break;
-            case exter_ports.J6:
-                SensorGray_pin = DigitalPin.P15;
+            case exter_ports1.J6:
+                SensorGray_SCL = DigitalPin.P15;
+                SensorGray_SDA = DigitalPin.P16;
                 break;
             default:
                 break;
         }
-        pins.digitalWritePin(SensorGray_pin, 1);
+        pins.digitalWritePin(SensorGray_SCL, 1);
+        pins.digitalReadPin(SensorGray_SDA);
+
+        SensorGray_Read();
+    }
+
+    /**
+     * Set the value as comparsion for five GraySensors
+     * @param CmpNum0 the comparsion fo channel0 GraySensors, eg: 500
+     * @param CmpNum1 the comparsion fo channel1 GraySensors, eg: 500
+     * @param CmpNum2 the comparsion fo channel2 GraySensors, eg: 500
+     * @param CmpNum3 the comparsion fo channel3 GraySensors, eg: 500
+     * @param CmpNum4 the comparsion fo channel4 GraySensors, eg: 500
+     */
+    //% blockId=SetCmpValue
+    //% block="set comparsion numbers from left to right as %CmpNum0|%CmpNum1|%CmpNum2|%CmpNum3|%CmpNum4|"
+    //% group=WalkLine
+    //% inlineInputMode=inline
+    export function SetCmpValue(CmpNum0: number, CmpNum1: number, CmpNum2: number, CmpNum3: number, CmpNum4: number) {
+        CmpValue_BUF[0] = CmpNum0;
+        CmpValue_BUF[1] = CmpNum1;
+        CmpValue_BUF[2] = CmpNum2;
+        CmpValue_BUF[3] = CmpNum3;
+        CmpValue_BUF[4] = CmpNum4;
+    }
+
+    /**
+     * Search the line in a specific number of intersections
+     * @param MaxSpeed the maxinum of speed, eg: 255
+     * @param LineNum the number of intersection, eg: 1
+     * @param EndTime the time after crossing the intersections, eg: 0.25
+     */
+    //% blockId=RunWalkLine
+    //% block="Max speed%MaxSpeed| Search %LineNum| intersection in %EndTime| seconds"
+    //% group=WalkLine
+    //% MaxSpeed.min=0 MaxSpeed.max=255
+    export function RunWalkLine(MaxSpeed: number, LineNum: number, EndTime: number) {
+        let temp: number;
+
+        for (let i = 0; i < LineNum; i++) {
+            WalkLineFunction(MaxSpeed);
+            basic.pause(2);
+        }
+
+        temp = EndTime * 100;
+        while (temp--) {
+            WalkReadValue();
+            WalkLineMotorControl(MaxSpeed);
+
+            //basic.pause(3);
+        }
+
+        LeftMotorSpeed(-255);	//反正
+        RightMotorSpeed(-255);
+        basic.pause(20);
+        LeftMotorSpeed(0);	//停止
+        RightMotorSpeed(0);
+    }
+
+    /**
+     * Search line in a specific time
+     * @param MaxSpeed the maxinum of speed, eg: 255
+     * @param Cnt the time searching, eg: 5
+     */
+    //% blockId=RunWalkLine_Timer
+    //% block="Max speed%MaxSpeed| Search line in %Cnt| seconds"
+    //% group=WalkLine
+    //% MaxSpeed.min=0 MaxSpeed.max=255
+    export function RunWalkLine_Timer(MaxSpeed: number, Cnt: number) {
+        let i = Cnt * 56;
+
+        while (i--) {
+            WalkReadValue();
+            WalkLineMotorControl(MaxSpeed);
+            basic.pause(1);
+        }
+
+        LeftMotorSpeed(0);	//停止
+        RightMotorSpeed(0);
+    }
+
+    /**
+     * Search line in turn-left mode
+     * @param MaxSpeed the maxinum of speed, eg: 255
+     * @param LineNum the number of intersection, eg: 1
+     */
+    //% blockId=RunWalkLine_Turn_Left
+    //% block="Max speed %MaxSpeed| Turn left after %LineNum| intersections"
+    //% group=WalkLine
+    //% MaxSpeed.min=0 MaxSpeed.max=255
+    export function RunWalkLine_Turn_Left(MaxSpeed: number, LineNum: number) {
+        let i = 0;
+        let j = LineNum;
+
+        while (j--) {
+            LeftMotorSpeed(MaxSpeed * -1);
+            RightMotorSpeed(MaxSpeed);
+
+            i = 0;
+            if (WalkReadValue() != 0) {	//判断车在线上														
+                while (1) {
+                    basic.pause(1);
+                    WalkReadValue();
+                    if (DATA_BUF[4] < CmpValue_BUF[4]) {
+                        if (++i > 4)
+                            break;
+                    } else
+                        i = 0;
+                }
+            }
+
+            i = 0;
+            while (1) {
+                basic.pause(1);
+                WalkReadValue();
+                if (DATA_BUF[2] < CmpValue_BUF[2]) {
+                    if (++i > 4)
+                        break;
+                }
+                else
+                    i = 0;
+            }
+
+            if (j === 0) {
+                LeftMotorSpeed(MaxSpeed);  //反转实现急停
+                RightMotorSpeed(1 - MaxSpeed);
+                basic.pause(20);
+            }
+        }
+
+        LeftMotorSpeed(0);	//停止
+        RightMotorSpeed(0);
+    }
+
+    /**
+     * Search line in turn-right mode
+     * @param MaxSpeed the maxinum of speed, eg: 255
+     * @param LineNum the number of intersection, eg: 1
+     */
+    //% blockId=RunWalkLine_Turn_Right
+    //% block="Max speed %MaxSpeed| Turn right after %LineNum| intersections"
+    //% group=WalkLine
+    //% MaxSpeed.min=0 MaxSpeed.max=255
+    export function RunWalkLine_Turn_Right(MaxSpeed: number, LineNum: number) {
+        let i = 0;
+        let j = LineNum;
+
+        while (j--) {
+            LeftMotorSpeed(MaxSpeed);
+            RightMotorSpeed(MaxSpeed * -1);
+
+            i = 0;
+            if (WalkReadValue() != 0) {	//判断车在线上
+                while (1) {
+                    basic.pause(1);
+                    WalkReadValue();
+                    if (DATA_BUF[0] < CmpValue_BUF[0]) {
+                        if (++i > 4)
+                            break;
+                    } else
+                        i = 0;
+                }
+            }
+            i = 0;
+            while (1) {
+                basic.pause(1);
+                WalkReadValue();
+                if (DATA_BUF[2] < CmpValue_BUF[2]) {
+                    if (++i > 4)
+                        break;
+                }
+                else
+                    i = 0;
+            }
+            if (j == 0) {
+                LeftMotorSpeed(1 - MaxSpeed);
+                RightMotorSpeed(MaxSpeed);
+                basic.pause(20);
+            }
+        }
+
+        LeftMotorSpeed(0);	//停止
+        RightMotorSpeed(0);
+    }
+
+    function WalkReadValue(): number {
+        let Temp = 0;
+        SensorGray_Read();
+
+        if (DATA_BUF[0] < CmpValue_BUF[0]) {
+            Temp++;
+        }
+        if (DATA_BUF[1] < CmpValue_BUF[1]) {
+            Temp++;
+        }
+        if (DATA_BUF[2] < CmpValue_BUF[2]) {
+            Temp++;
+        }
+        if (DATA_BUF[3] < CmpValue_BUF[3]) {
+            Temp++;
+        }
+        if (DATA_BUF[4] < CmpValue_BUF[4]) {
+            Temp++;
+        }
+
+        return Temp;
+    }
+
+    function WalkLineMotorControl(MaxSpeed: number) {
+        if (DATA_BUF[1] < CmpValue_BUF[1]) {  //左边触发黑线 		
+            LeftMotorSpeed(0);
+            RightMotorSpeed(MaxSpeed);
+        }
+        else if (DATA_BUF[3] < CmpValue_BUF[3]) { //右边触发黑线 
+            LeftMotorSpeed(MaxSpeed);
+            RightMotorSpeed(0);
+        }
+        /*		else if(DATA_BUF[0] < CmpValue_BUF[0]) {   //最左边触发黑线     			
+                    LeftMotorSpeed(MaxSpeed);
+                    RightMotorSpeed(MaxSpeed);
+                    }
+                    else if(DATA_BUF[4] < CmpValue_BUF[4]) {   //最右边触发黑线   		
+                        LeftMotorSpeed(MaxSpeed);
+                        RightMotorSpeed(MaxSpeed);
+                    }*/
+        else {
+            LeftMotorSpeed(MaxSpeed);
+            RightMotorSpeed(MaxSpeed);
+        }
+    }
+
+    function WalkLineFunction(MaxSpeed: number) {
+        let Count = 0;
+
+        while (1) {
+            WalkReadValue();
+
+            if (!(DATA_BUF[0] < CmpValue_BUF[0] || DATA_BUF[4] < CmpValue_BUF[4])) //判断不在路口
+            {
+                if (Count > 4) {//判断是否已经检测到黑线
+                    //delay(50);
+                    return;
+                }
+                else
+                    Count = 0;
+            }
+            else	//在黑线上
+                Count++;
+
+            WalkLineMotorControl(MaxSpeed);
+            basic.pause(1);
+        }
+    }
+
+    function RightMotorSpeed(s: number) {
+        // s ranges from 0 to 255
+        if (s >= 0)
+            exter_motor_drive(motor_ports.J8, s * 3, motor_dir.REV)
+        else {
+            s = 0 - s;  //转为整数
+            exter_motor_drive(motor_ports.J8, s * 3, motor_dir.FWD)
+        }
+    }
+
+    function LeftMotorSpeed(s: number) {
+        // s ranges from 0 to 255
+        if (s >= 0)
+            exter_motor_drive(motor_ports.J7, s * 3, motor_dir.FWD)
+        else {
+            s = 0 - s;  //转为整数
+            exter_motor_drive(motor_ports.J7, s * 3, motor_dir.REV)
+        }
     }
 
     function SensorGray_Read() {
         let DATA = 0;
         let i = 0, j = 0;
-        for (j = 0; j < 6; j++) {
-            for (i = 0; i < 16; i++) {
-                pins.digitalWritePin(SensorGray_pin, 0);     //设置低电平
-                pins.digitalWritePin(SensorGray_pin, 1);		//设置高电平
 
-                //DATA <<= 1;
-                if (pins.digitalReadPin(SensorGray_pin)) //判断是否高电平
+        pins.digitalWritePin(SensorGray_SCL, 0);            //开始接受数据
+        control.waitMicros(20);
+
+        for (j = 0; j < 5; j++) {
+            DATA = 0;
+            for (i = 0; i < 16; i++) {
+                pins.digitalWritePin(SensorGray_SCL, 0);    //设置低电平
+                control.waitMicros(20);
+
+                DATA <<= 1;
+                if (pins.digitalReadPin(SensorGray_SDA))    //判断是否高电平
                 {
                     DATA += 1;
                 }
-                DATA <<= 1;
-                pins.digitalWritePin(SensorGray_pin, 1);	//设置输出模式	
+                //DATA <<= 1;
+                //control.waitMicros(20);
+
+                pins.digitalWritePin(SensorGray_SCL, 1);	//设置高电平
+                control.waitMicros(10);
             }
-            DATA &= 0x7fff;
+            DATA &= 0x3ff;
             DATA_BUF[j] = DATA;  //数据存入缓存
         }
+        basic.pause(1)
     }
 
     /**
@@ -137,8 +537,7 @@ namespace Coolguy_basic {
     //% blockId=SensorGray_ReadOne
     //% block="get the value from No.%num| Sensor"
     //% num.min=1 num.max=5
-    //% group=SensorGray
-    //% group=GraySensors
+    //% group=WalkLine
     export function SensorGray_ReadOne(num: number): number {
         SensorGray_Read();
         return DATA_BUF[num - 1];
@@ -753,67 +1152,33 @@ namespace Coolguy_basic {
     }
 
     /**
-     * Car turn left(both forward)
-     * @param difspeed the differential of wheels, eg: 400
+     * Car turn left(wheels reverse)
+     * @param speed the speed of wheels, eg: 500
      * left: IO5/11
      * right: IO8/12
      */
     //% blockId=coolguy_extermotor_left
-    //% block="let car turn left %difspeed|(differential)" 
-    //% difspeed.min=0 difspeed.max=1023
+    //% block="let car turn left %speed|" 
+    //% speed.min=0 speed.max=1023
     //% group=Motors
-    export function exter_motor_left(difspeed: number): void {
-        if (difspeed > 1000)
-            difspeed = 1000;
-        exter_motor_drive(motor_ports.J7, 500-difspeed/2, motor_dir.FWD)
-        exter_motor_drive(motor_ports.J8, 500+difspeed/2, motor_dir.REV)
-    }
-
-    /**
-     * Car turn left(wheels reverse)
-     * @param difspeed the differential of wheels, eg: 400
-     * left: IO5/11
-     * right: IO8/12
-     */
-    //% blockId=coolguy_extermotor_left1
-    //% block="let car(wheels reverse) turn left %difspeed|(differential)" 
-    //% difspeed.min=0 difspeed.max=1023
-    //% group=Motors
-    export function exter_motor_left1(difspeed: number): void {
-        exter_motor_drive(motor_ports.J7, difspeed, motor_dir.REV)
-        exter_motor_drive(motor_ports.J8, difspeed, motor_dir.REV)
-    }
-
-    /**
-     * Car turn right(both forward)
-     * @param difspeed the differential of wheels, eg: 400
-     * left: IO5/11
-     * right: IO8/12
-     */
-    //% blockId=coolguy_extermotor_right
-    //% block="let car turn right %difspeed|(differential)" 
-    //% difspeed.min=0 difspeed.max=1023
-    //% group=Motors
-    export function exter_motor_right(difspeed: number): void {
-        if (difspeed > 1000)
-            difspeed = 1000;
-        exter_motor_drive(motor_ports.J7, 500+difspeed/2, motor_dir.FWD)
-        exter_motor_drive(motor_ports.J8, 500-difspeed/2, motor_dir.REV)
+    export function exter_motor_left(speed: number): void {
+        exter_motor_drive(motor_ports.J7, speed, motor_dir.REV)
+        exter_motor_drive(motor_ports.J8, speed, motor_dir.REV)
     }
 
     /**
      * Car turn right(wheels reverse)
-     * @param difspeed the differential of wheels, eg: 400
+     * @param speed the speed of wheels, eg: 500
      * left: IO5/11
      * right: IO8/12
      */
-    //% blockId=coolguy_extermotor_right1
-    //% block="let car(wheels reverse) turn right %difspeed|(differential)" 
-    //% difspeed.min=0 difspeed.max=1023
+    //% blockId=coolguy_extermotor_right
+    //% block="let car turn right %speed|" 
+    //% speed.min=0 speed.max=1023
     //% group=Motors
-    export function exter_motor_right1(difspeed: number): void {
-        exter_motor_drive(motor_ports.J7, difspeed, motor_dir.FWD)
-        exter_motor_drive(motor_ports.J8, difspeed, motor_dir.FWD)
+    export function exter_motor_right(speed: number): void {
+        exter_motor_drive(motor_ports.J7, speed, motor_dir.FWD)
+        exter_motor_drive(motor_ports.J8, speed, motor_dir.FWD)
     }
 
     /**
