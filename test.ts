25fb2f6148ae92ae5563d46b1794ad39e00bf718
CoolGuy-official <34023728+CoolGuy-official@users.noreply.github.com>
2021-01-06 20:37:02 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,18 @@
-// tests go here; this will not be compiled when this package is used as a library
+{
+  // motor test
+  Coolguy_basic.exter_motor_drive(motor_ports.J7, 255, motor_dir.FWD)
+  Coolguy_basic.exter_motor_drive(motor_ports.J8, 255, motor_dir.FWD)
+  basic.pause(1000)
+  Coolguy_basic.exter_motor_drive(motor_ports.J7, 127, motor_dir.FWD)
+  Coolguy_basic.exter_motor_drive(motor_ports.J8, 127, motor_dir.FWD)
+  basic.pause(1000)
+  Coolguy_basic.exter_motor_drive(motor_ports.J7, 255, motor_dir.REV)
+  Coolguy_basic.exter_motor_drive(motor_ports.J8, 255, motor_dir.REV)
+  basic.pause(1000)
+  Coolguy_basic.exter_motor_drive(motor_ports.J7, 127, motor_dir.REV)
+  Coolguy_basic.exter_motor_drive(motor_ports.J8, 127, motor_dir.REV)
+  basic.pause(1000)
+  
+  // next
+  
+}
